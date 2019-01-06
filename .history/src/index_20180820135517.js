c3bd32fe25c6648bd911853f52af97f2d746ce26
chenc <double_cl@163.com>
2019-01-06 19:54:10 +0800

    delete .history

--- .history/src/index_20180820135517.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Hello from './component/Hello'
-ReactDOM.render(
-  <Hello />,
-  document.getElementById('root')
-)
--- .history/src/index_20190106193455.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app';
-
-const render = Component => ReactDOM.render(<Component />, document.getElementById('root'));
-
-render(App);
-if (module.hot) module.hot.accept('./app.js', () => render(App));
